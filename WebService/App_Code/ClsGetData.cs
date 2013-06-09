5c01dd0b370b590800fe23d5061ba1cbcc7d918c
XiangC0HX79 <hx_smm@Hotmail.com>
2013-06-09 11:10:43 +0800

    WebService

---
+++ WebService/App_Code/ClsGetData.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+
+using System.Data.Common;
+
+/// <summary>
+/// 类：访问数据库
+/// </summary>
+public class ClsGetData
+{
+    private string _factoryType = "System.Data.OracleClient";
+    private string _connectionString;
+
+    private string _errorString = "";
+    public string ErrorString
+    {
+        get { return _errorString; }
+    }
+    
+    public DataTable ErrorTable
+    {
+        get
+        {
+            DataTable dataTable = new DataTable("Error");
+            DataColumn column = new DataColumn("ErrorInfo", Type.GetType("System.String"));
+            dataTable.Columns.Add(column);
+
+            DataRow row = dataTable.NewRow();
+            row["ErrorInfo"] = _errorString;
+            dataTable.Rows.Add(row);
+
+            return dataTable;
+        }
+    }
+
+    public DataTable CountTable(int count)
+    {
+        DataTable dataTable = new DataTable("Count");
+        DataColumn column = new DataColumn("Count", Type.GetType("System.Int32"));
+        dataTable.Columns.Add(column);
+        
+        DataRow row = dataTable.NewRow();
+        row["Count"] = count;
+        dataTable.Rows.Add(row);
+
+        return dataTable;
+    }
+
+    public ClsGetData()
+    {
+        _connectionString = "USER ID=" + System.Configuration.ConfigurationManager.AppSettings["GISDB_USER"]
+                            + ";PASSWORD=" + System.Configuration.ConfigurationManager.AppSettings["GISDB_PASS"]
+                            +";DATA SOURCE=(DESCRIPTION =(ADDRESS_LIST =(ADDRESS = (PROTOCOL = TCP)(HOST = "
+                            + System.Configuration.ConfigurationManager.AppSettings["GISDB_SERVER"]
+                            +")(PORT = 1521)))(CONNECT_DATA =(SERVICE_NAME = "
+                            + System.Configuration.ConfigurationManager.AppSettings["GISDB_SERVICE"] + ")))";
+    }
+
+    public ClsGetData(string connectionString)
+	{
+        _connectionString = connectionString;
+	}
+
+    public ClsGetData(string factoryType, string connectionString)
+    {
+        _connectionString = connectionString;
+        _factoryType = factoryType;
+    }
+
+    public DataTable GetTable(string selectsql)
+    {
+        DbProviderFactory dbProviderFactory = DbProviderFactories.GetFactory(_factoryType);
+        IDbConnection dbConnection = dbProviderFactory.CreateConnection();
+        dbConnection.ConnectionString = _connectionString;
+
+        IDbCommand dbCommand = dbProviderFactory.CreateCommand();
+        dbCommand.CommandText = selectsql;
+        dbCommand.Connection = dbConnection;
+        
+        IDbDataAdapter dataAdapter = dbProviderFactory.CreateDataAdapter();
+        dataAdapter.SelectCommand = dbCommand;
+        
+        DataSet dataSet = new DataSet();
+        try
+        {
+            dataAdapter.Fill(dataSet);
+        }
+        catch (Exception errMsg)
+        {
+            _errorString = errMsg.Message;
+            return ErrorTable;
+        }
+        if (dataSet.Tables.Count != 1)
+            return ErrorTable;
+        else
+            return dataSet.Tables[0];
+    }
+
+    public object GetValue(string selectsql)
+    {
+        DbProviderFactory dbProviderFactory = DbProviderFactories.GetFactory(_factoryType);
+        IDbConnection dbConnection = dbProviderFactory.CreateConnection();
+        dbConnection.ConnectionString = _connectionString;
+
+        IDbCommand dbCommand = dbProviderFactory.CreateCommand();
+        dbCommand.CommandText = selectsql;
+        dbCommand.Connection = dbConnection;
+
+        object result;
+        try
+        {
+            dbConnection.Open();
+            result = dbCommand.ExecuteScalar();
+
+        }
+        catch (Exception errMsg)
+        {
+            _errorString = errMsg.Message;
+            result = null;
+        }
+
+        if (dbConnection.State == ConnectionState.Open)
+        {
+            dbConnection.Close();
+            dbConnection.Dispose();
+        }
+
+        return result;
+    }
+
+    public DataTable SetTable(string sql)
+    {
+        int iCount;
+
+        DbProviderFactory dbProviderFactory = DbProviderFactories.GetFactory(_factoryType);
+        IDbConnection dbConnection = dbProviderFactory.CreateConnection();
+        dbConnection.ConnectionString = _connectionString;
+
+        IDbCommand dbCommand = dbProviderFactory.CreateCommand();
+        dbCommand.CommandText = sql;
+        dbCommand.Connection = dbConnection;
+
+        try
+        {
+            dbConnection.Open();
+            iCount = dbCommand.ExecuteNonQuery();
+        }
+        catch (Exception errMsg)
+        {
+            _errorString = errMsg.Message;
+            return ErrorTable;
+            //iCount = 0;
+        }
+
+        if (dbConnection.State == ConnectionState.Open)
+        {
+            dbConnection.Close();
+            dbConnection.Dispose();
+        }
+
+        return CountTable(iCount);
+    }
+
+    public DataTable ExcuteNoQuery(string sql)
+    {
+        string[] sqls = sql.Split(';');
+
+        DbProviderFactory dbProviderFactory = DbProviderFactories.GetFactory(_factoryType);
+        IDbConnection dbConnection = dbProviderFactory.CreateConnection();
+        dbConnection.ConnectionString = _connectionString;
+
+        IDbTransaction dbTrans = null;
+        int iCount = 0;
+        try
+        {
+            dbConnection.Open();
+
+            dbTrans = dbConnection.BeginTransaction();
+
+            foreach (string p in sqls)
+            {
+                if (p != "")
+                {
+                    IDbCommand dbCommand = dbProviderFactory.CreateCommand();
+                    dbCommand.CommandText = p;
+                    dbCommand.Connection = dbConnection;
+                    dbCommand.Transaction = dbTrans;
+                    iCount += dbCommand.ExecuteNonQuery();
+                }
+            }
+
+            dbTrans.Commit();
+        }
+        catch (Exception errMsg)
+        {
+            if (dbConnection.State == ConnectionState.Open)
+                dbTrans.Rollback();
+
+            _errorString = errMsg.Message;
+            return ErrorTable;
+            //iCount = 0;
+        }
+
+        if (dbConnection.State == ConnectionState.Open)
+        {
+            dbConnection.Close();
+            dbConnection.Dispose();
+        }
+
+        return CountTable(iCount);
+    }
+}
--- WebService/App_Code/Service.cs
+++ WebService/App_Code/Service.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Services;
 
@@ -8,18 +7,22 @@ using System.Data;
 
 [WebService(Namespace = "http://tempuri.org/")]
 [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
-// 若要允许使用 ASP.NET AJAX 从脚本中调用此 Web 服务，请取消对下行的注释。
-// [System.Web.Script.Services.ScriptService]
 public class Service : System.Web.Services.WebService
 {
-    public Service () {
+    private String conStr = "";
+
+    public Service()
+    {
 
         //如果使用设计的组件，请取消注释以下行 
         //InitializeComponent(); 
+
+        conStr = System.Configuration.ConfigurationManager.AppSettings["CONSTR"];
     }
 
     [WebMethod]
-    public string HelloWorld() {
+    public string HelloWorld()
+    {
         return "Hello World";
     }
 
@@ -39,112 +42,229 @@ public class Service : System.Web.Services.WebService
     }
 
     [WebMethod]
-    public DataTable InitSurrounding() 
+    public DataTable InitBuild(String buildName)
     {
-        DataTable table = new DataTable("Table");
-        table.Columns.Add(new DataColumn("TMB_ID", Type.GetType("System.String")));
-        table.Columns.Add(new DataColumn("TMB_SurroundingBitmap", Type.GetType("System.String")));
+        ClsGetData clsGetData = new ClsGetData("System.Data.SqlClient", conStr);
 
-        DataRow row = table.NewRow();
-        row["TMB_ID"] = "1";
-        row["TMB_SurroundingBitmap"] = "../assets/image/surrounding.png";
-        table.Rows.Add(row);
+        //String sql = "Select * FROM T_MainBulid LEFT JOIN T_MainBulid_Append ON T_MainBulid.TMB_ID = T_MainBulid_Append.TMB_ID "
+        //                        + "WHERE TMB_Name = '" + buildName + "'";
+        String sql = "Select * FROM T_MainBulid "
+                     + "LEFT JOIN T_MainBulid_Append ON T_MainBulid.TMB_ID = T_MainBulid_Append.TMB_ID "
+                     + "LEFT JOIN T_FireInformation ON T_MainBulid.TMB_ID = T_FireInformation.TMB_ID "
+                     + "WHERE TMB_Name = '上海东方艺术中心'";
 
-        return table;
+        DataTable result = clsGetData.GetTable(sql);
+
+        return result;
     }
 
     [WebMethod]
-    public DataTable InitKeyPoint(String buildID)
+    public DataTable InitCommandingHeights(String buildID)
     {
-        DataTable table = new DataTable("Table");
-        table.Columns.Add(new DataColumn("ID", Type.GetType("System.String")));
-        table.Columns.Add(new DataColumn("X", Type.GetType("System.String")));
-        table.Columns.Add(new DataColumn("Y", Type.GetType("System.String")));
+        ClsGetData clsGetData = new ClsGetData("System.Data.SqlClient", conStr);
 
-        DataRow row = table.NewRow();
-        row["ID"] = "1";
-        row["X"] = "50";
-        row["Y"] = "50";
-        table.Rows.Add(row);
+        String sql = "Select * FROM T_CommandingHeights LEFT JOIN T_CommandingHeightsPIC ON T_CommandingHeights.TCH_ID = T_CommandingHeightsPIC.TCH_ID "
+                    + "WHERE T_CommandingHeightsPIC.TMB_ID = " + buildID + " AND T_CommandingHeightsPIC.T_ComType = 1";
 
-        row = table.NewRow();
-        row["ID"] = "1";
-        row["X"] = "100";
-        row["Y"] = "100";
-        table.Rows.Add(row);
+        DataTable result = clsGetData.GetTable(sql);
 
-        return table;
+        return result;
     }
 
     [WebMethod]
-    public DataTable InitControlRange()
+    public DataTable InitCommandingHeightsPIC(String TCH_ID)
     {
-        DataTable table = new DataTable("Table");
-        table.Columns.Add(new DataColumn("TMB_ControlRangeBitmap", Type.GetType("System.String")));
-        table.Columns.Add(new DataColumn("X", Type.GetType("System.String")));
-        table.Columns.Add(new DataColumn("Y", Type.GetType("System.String")));
+        ClsGetData clsGetData = new ClsGetData("System.Data.SqlClient", conStr);
 
-        DataRow row = table.NewRow();
-        row["TMB_ControlRangeBitmap"] = "../assets/image/gogopher.jpg";
-        row["X"] = "50";
-        row["Y"] = "50";
-        table.Rows.Add(row);
+        String sql = "Select * FROM T_CommandingHeightsPIC WHERE TCH_ID = " + TCH_ID + " AND T_ComType = 2";
 
-        return table;
+        DataTable result = clsGetData.GetTable(sql);
+
+        return result;
     }
 
     [WebMethod]
-    public DataTable InitBuild()
+    public DataTable InitClosedhandles(String buildID)
     {
-        DataTable table = new DataTable("Table");
-        table.Columns.Add(new DataColumn("TMB_PicPath", Type.GetType("System.String")));
+        ClsGetData clsGetData = new ClsGetData("System.Data.SqlClient", conStr);
 
-        DataRow row = table.NewRow();
-        row["TMB_PicPath"] = "../assets/image/plant4.jpg";
-        table.Rows.Add(row);
+        String sql = "Select * FROM T_Closedhandles WHERE  TMB_ID =" + buildID;
 
-        return table;
+        DataTable result = clsGetData.GetTable(sql);
+
+        return result;
+    }
+
+    [WebMethod]
+    public DataTable InitClosedhandlesPIC(String T_ClosedhandlesID)
+    {
+        ClsGetData clsGetData = new ClsGetData("System.Data.SqlClient", conStr);
+
+        String sql = "Select * FROM T_ClosedhandlesPic Where T_ClosedhandlesID = " + T_ClosedhandlesID;
+
+        DataTable result = clsGetData.GetTable(sql);
+
+        return result;
+    }
+    
+    [WebMethod]
+    public DataTable InitTraffic(String TMB_ID)
+    {
+        ClsGetData clsGetData = new ClsGetData("System.Data.SqlClient", conStr);
+
+        String sql = "Select * FROM T_Traffic "
+                     + "LEFT JOIN T_TrafficPic ON T_Traffic.T_TrafficID = T_TrafficPic.T_TrafficID "
+                     + "WHERE T_TrafficPic.TMB_ID = " + TMB_ID;
+
+        DataTable result = clsGetData.GetTable(sql);
+
+        return result;
+    }
+    
+    [WebMethod]
+    public DataTable InitHazard(String TMB_ID)
+    {
+        ClsGetData clsGetData = new ClsGetData("System.Data.SqlClient", conStr);
+
+        String sql = "Select * FROM T_Hazard "
+                     + "LEFT JOIN T_HazardPic ON T_Hazard.T_HazardID = T_HazardPic.T_HazardID "
+                     + "WHERE T_Hazard.TMB_ID = " + TMB_ID;
+
+        DataTable result = clsGetData.GetTable(sql);
+
+        return result;
+    }
+
+    [WebMethod]
+    public DataTable InitKeyUnits(String TMB_ID)
+    {
+        ClsGetData clsGetData = new ClsGetData("System.Data.SqlClient", conStr);
+
+        String sql = " Select * FROM T_KeyUnits "
+                    + "LEFT JOIN T_KeyUnitsPic ON T_KeyUnits.T_KeyUnitsID = T_KeyUnitsPic.T_KeyUnitsID "
+                    + "WHERE T_KeyUnits.TMB_ID = " + TMB_ID;
+
+        DataTable result = clsGetData.GetTable(sql);
+
+        return result;
+    }
+    
+    [WebMethod]
+    public DataTable InitScenting(String TMB_ID)
+    {
+        ClsGetData clsGetData = new ClsGetData("System.Data.SqlClient", conStr);
+
+        String sql = "Select * FROM T_Scenting "
+                     + "LEFT JOIN T_ScentingImg ON T_Scenting.T_ScentingID = T_ScentingImg.T_ScentingID "
+                     + "WHERE T_Scenting.TMB_ID = " + TMB_ID;
+
+        DataTable scenting = clsGetData.GetTable(sql);
+
+        sql = "Select * FROM T_ScentingImg "
+                    + "WHERE TMB_ID = " + TMB_ID;
+
+        DataTable img = clsGetData.GetTable(sql);
+
+        foreach (DataRow k in img.Rows)
+        {
+            var s = k["T_ScentingimgOwen"].ToString().Split(',');
+            foreach (var i in s)
+            {
+                foreach(var j in scenting.Select("T_ScentingID = " + i))
+                {
+                    j["T_ScentingImgName"] = k["T_ScentingImgName"];
+                    j["T_ScentingimgPath"] = k["T_ScentingimgPath"];
+                }
+            }
+        }
+
+        return scenting;
+    }
+    
+    [WebMethod]
+    public DataTable InitTatics(String TMB_ID)
+    {
+        ClsGetData clsGetData = new ClsGetData("System.Data.SqlClient", conStr);
+
+        String sql = "Select * FROM T_TacticalPoints WHERE TMB_ID = " + TMB_ID;
+
+        DataTable result = clsGetData.GetTable(sql);
+
+        return result;
     }
 
     [WebMethod]
     public DataTable InitFloor(String buildID)
     {
-        DataTable table = new DataTable("Table");
-        table.Columns.Add(new DataColumn("T_FloorID", Type.GetType("System.String")));
-        table.Columns.Add(new DataColumn("T_FloorName", Type.GetType("System.String")));
-        table.Columns.Add(new DataColumn("T_FloorPicPath", Type.GetType("System.String")));
+        ClsGetData clsGetData = new ClsGetData("System.Data.SqlClient", conStr);
 
-        DataRow row = table.NewRow();
-        row["T_FloorID"] = "1";
-        row["T_FloorName"] = "测试";
-        row["T_FloorPicPath"] = "../assets/image/gogopher.jpg";
+        DataTable result = clsGetData.GetTable("SELECT T_Floor.T_FloorID,T_FloorName,T_FloorPicPath,T_FloorScale,T_FloorX,T_FloorY,T_FloorXRotation,T_FloorYRotation,T_FloorZRotation,T_FloorAlpha FROM T_Floor LEFT JOIN T_FloorPos ON T_Floor.T_FloorID = T_FloorPos.T_FloorID AND T_Floor.TMB_ID = T_FloorPos.TMB_ID WHERE T_Floor.TMB_ID = " + buildID + " ORDER BY T_Floor.T_Floorsque DESC");
 
-        table.Rows.Add(row);
-
-        return table;
-    }    
-
-    [WebMethod]
-    public DataTable InitComponent(String buildID,String floorID)
-    {
-        DataTable table = new DataTable("Table");
-        table.Columns.Add(new DataColumn("T_FloorDetailID", Type.GetType("System.String")));
-        table.Columns.Add(new DataColumn("T_FloorDetailName", Type.GetType("System.String")));
-        table.Columns.Add(new DataColumn("T_FloorPicimgPath", Type.GetType("System.String")));
-
-        return table;
+        return result;
     }
 
     [WebMethod]
-    public DataTable InitContingencyPlans()
+    public DataTable SaveFloor(String buildID, String data)
     {
-        DataTable table = new DataTable("Table");
-        table.Columns.Add(new DataColumn("TMB_ContingencyPlans", Type.GetType("System.String")));
+        ClsGetData clsGetData = new ClsGetData("System.Data.SqlClient", conStr);
 
-        DataRow row = table.NewRow();
-        row["TMB_ContingencyPlans"] = "../assets/doc/东方明珠应急预案.doc";
-        table.Rows.Add(row);
+        String sql = "";
 
-        return table;
+        foreach (String dataFloor in data.Split('@'))
+        {
+            String[] floorPos = dataFloor.Split(';');
+            if (floorPos[0] != "")
+            {
+                sql += "DELETE FROM T_FloorPos WHERE TMB_ID = " + buildID + " AND T_FloorID = " + floorPos[0] + ";";
+                sql += "INSERT T_FloorPos (TMB_ID,T_FloorID,T_FloorScale,T_FloorX,T_FloorY,T_FloorXRotation,T_FloorYRotation,T_FloorZRotation,T_FloorAlpha) VALUES (" + buildID + "," + floorPos[0] + "," + floorPos[1] + "," + floorPos[2] + "," + floorPos[3] + "," + floorPos[4] + "," + floorPos[5] + "," + floorPos[6] + "," + floorPos[7] + ");";
+            }
+        }
+
+        DataTable result = clsGetData.ExcuteNoQuery(sql);
+
+        return result;
+    }
+
+    [WebMethod]
+    public DataTable InitComponent(String buildID, String floorID)
+    {
+        ClsGetData clsGetData = new ClsGetData("System.Data.SqlClient", conStr);
+
+        DataTable result = clsGetData.GetTable("SELECT T_FloorDetail.T_FloorID,T_FloorDetailchildfloor,T_FloorDetailID,T_FloorDetailName,T_FloorPicimgPath,T_FloorDetailX,T_FloorDetailY,T_FloorDetailType FROM T_FloorDetail,T_FloorPic WHERE T_FloorDetail.TMB_ID = " + buildID + " AND T_FloorDetail.T_FloorPicID = T_FloorPic.T_FloorPicID ORDER BY T_FloorDetailID DESC");
+
+        for (int i = result.Rows.Count - 1; i >= 0; i--)
+        {
+            DataRow row = result.Rows[i];
+            String T_FloorID = row["T_FloorID"].ToString();
+            if (floorID != T_FloorID)
+            {
+                Boolean b = false;
+
+                String[] childfloors = row["T_FloorDetailchildfloor"].ToString().Split(',');
+                foreach (String childfloor in childfloors)
+                {
+                    if (floorID == childfloor)
+                    {
+                        b = true;
+                        break;
+                    }
+                }
+
+                if (!b)
+                    result.Rows.Remove(row);
+            }
+        }
+
+        return result;
+    }
+
+    [WebMethod]
+    public DataTable InitComponentMedia(String componentID)
+    {
+        ClsGetData clsGetData = new ClsGetData("System.Data.SqlClient", conStr);
+
+        DataTable result = clsGetData.GetTable("SELECT * FROM T_FloorMedia WHERE T_FloorDetailID = " + componentID);
+
+        return result;
     }
 }
